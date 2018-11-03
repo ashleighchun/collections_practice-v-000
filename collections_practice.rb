24f2276e61b6fc3695219b8ac8e006186bc77407
Ashleigh Chun <ashleigh.chun@wework.com>
2018-11-03 22:50:47 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -51,8 +51,9 @@ end
 def find_a(array)
   character = gets.chomp!
   array.each do |e|
-  if e.include?(character)
-    return e
+    if e.include?(character)
+      return e
+    end
   end
 end
 
