a94335bb8fb6f0b5c46479f3d560112ee94aa08c
Ashleigh Chun <ashleigh.chun@wework.com>
2018-11-03 22:42:44 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,69 @@
 
+def sort_array_asc(array)
+  array.sort 
+end
+
+
+
+def sort_array_desc(array)
+  array.sort do |a, b|
+    if a == b 
+      0
+    elsif a > b 
+      -1
+    elsif a < b
+      1
+    end
+  end
+end
+
+
+
+def sort_array_char_count(array)
+  array.sort {|x, y| x.length <=> y.length}
+end 
+
+
+
+
+def swap_elements(array)
+  array[0], array[1], array[2] = array[0], array[2], array[1]
+end 
+
+
+
+
+def reverse_array(array)
+  array.reverse
+end
+
+
+
+def kesha_maker(array)
+    array.each do |x|
+      x[2] = '$'
+   end
+end 
+
+
+
+
+def find_a(array)
+  
+end
+
+
+
+
+#def sum_array(array)
+
+
+#end
+
+
+
+
+
+#def add_s(array)
+
+#end
